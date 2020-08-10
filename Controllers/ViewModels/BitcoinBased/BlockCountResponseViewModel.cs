11e19b67c21b90dfa16a55dbfd9cba78964ce00c
luusluus <lucienmartijn@gmail.com>
2020-08-10 17:22:36 +0200

    restructure

--- Controllers/ViewModels/BitcoinBased/BlockCountResponseViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace XRouter.Api.Controllers.ViewModels.BitcoinBased
-{
-    public class BlockCountResponseViewModel : XRouterBaseResponseViewModel
-    {
-        public int Reply { get; set; }
-    }
-}
--- Controllers/ViewModels/BitcoinBased/VoutViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace XRouter.Api.Controllers.ViewModels.BitcoinBased
-{
-    public class VoutViewModel
-    {
-        public decimal Value { get; set; }
-        public int N { get; set; }
-        public ScriptPubKeyViewModel ScriptPubKey { get; set; }
-    }
-}
